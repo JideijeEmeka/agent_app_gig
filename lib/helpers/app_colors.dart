e9a01c78dae6664acdd4ccd757250cc335f3441f
Emeka Jideije <jideije.emeka@gmail.com>
2021-12-10 21:05:17 +0100

    Create the register view

---
+++ lib/helpers/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const mainColor = Color(0xFFFFFFFF);
+  static const headerColor = Color(0xFFD78714);
+  static const subHeaderColor = Color(0xFF1F2B2E);
+}
--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,4 +1,6 @@
+import 'package:agent_app/helpers/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -11,7 +13,38 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("data here now")),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 23),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 86,
+            ),
+            Text(
+              "Register",
+              style: GoogleFonts.inter(
+                  textStyle: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.headerColor,
+                      fontStyle: FontStyle.normal)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 7),
+              child: Text(
+                "Step 1 of 3",
+                style: GoogleFonts.inter(
+                    textStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.subHeaderColor,
+                        fontStyle: FontStyle.normal)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
